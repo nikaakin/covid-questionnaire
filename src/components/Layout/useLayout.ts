3ad743bc31ca38219ad039f4e9002318e3d530d4
Nika Tsutskiridze <nikacuckiridze47@gmail.com>
2023-05-13 23:01:59 +0400

    chore : remove comments

--- src/components/Layout/useLayout.ts
+++ src/components/Layout/useLayout.ts
@@ -7,7 +7,6 @@ export const useLayout = () => {
   const navigate = useNavigate();
   const outlet = useOutlet();
 
-  // TODO  add validation to this functions after
   const previousPage = () => setCurrentPage(page - 1, navigate);
   const nextPage = () => setCurrentPage(page + 1, navigate);
 
--- src/helpers/getLocalStorageValues.ts
+++ src/helpers/getLocalStorageValues.ts
@@ -1,5 +1,4 @@
 export const getLocaleStorageValues = () => {
-  // TODO add type to formValues
   const formValues = JSON.parse(localStorage.getItem('form') || '{}');
   const currentPage = +(localStorage.getItem('currentPage') || '0');
   return {
