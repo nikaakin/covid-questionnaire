8b32a2d6348d56b8310cdcad9be8fe198c387333
Nika Tsutskiridze <nikacuckiridze47@gmail.com>
2023-05-19 01:59:16 +0400

    refactor: add custom hook to covid politics

--- src/pages/CovidPolitics/components/CovidPoliticsForm/CovidPoliticsForm.tsx
+++ src/pages/CovidPolitics/components/CovidPoliticsForm/CovidPoliticsForm.tsx
@@ -1,21 +1,15 @@
 import { RadioButton } from '@/components';
-import { covidPoliticsContext } from '@/context';
-import { useContext } from 'react';
-import { useWatch } from 'react-hook-form';
+import { useCovidPoliticsForm } from './useCovidPoliticsForm';
 
 export const CovidPoliticsForm = () => {
-  const { setCovidPoliticsData } = useContext(covidPoliticsContext);
-
-  const watchValues = useWatch({
-    name: ['had_vaccine', 'vaccination_stage', 'i_am_waiting'],
-  });
-  const firstAdditionalInput = watchValues[0] === 'true';
-  const secondAdditionalInput = watchValues[0] === 'false';
-  const thirdAdditionalInput =
-    watchValues[1] === 'first_dosage_and_not_registered_yet';
-  const fourthAdditionalInput =
-    watchValues[2] === 'had_covid_and_planning_to_be_vaccinated';
-  const fifthAdditionalInput = watchValues[2] === 'not_planning';
+  const {
+    firstAdditionalInput,
+    secondAdditionalInput,
+    thirdAdditionalInput,
+    fourthAdditionalInput,
+    fifthAdditionalInput,
+    setCovidPoliticsData,
+  } = useCovidPoliticsForm();
 
   return (
     <div
